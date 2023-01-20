41b3eb41387ed2bf496982c69305cbda674f62de
sapanmozammel <sapanmozammel@gmail.com>
2023-01-20 21:31:29 +0600

    Update Sidebar.js

--- src/components/common/dashboard/Sidebar.js
+++ src/components/common/dashboard/Sidebar.js
@@ -2,9 +2,10 @@ import { Fragment } from "react";
 import { Dialog, Transition } from "@headlessui/react";
 import { XMarkIcon } from "@heroicons/react/24/outline";
 import { routes } from "../../../routes";
-import { Link, NavLink } from "react-router-dom";
+import { Link, NavLink, useLocation } from "react-router-dom";
 
 const Sidebar = ({ sidebarOpen, setSidebarOpen }) => {
+  const { pathname } = useLocation();
   const landingLinks = routes.find((navLink) => navLink.layout === "user");
   const navLinks = landingLinks?.children?.length
     ? landingLinks?.children?.filter((navLink) => navLink?.navbar === true)
@@ -80,13 +81,12 @@ const Sidebar = ({ sidebarOpen, setSidebarOpen }) => {
                           <NavLink
                             key={navLink.name}
                             to={navLink?.path ?? "/user"}
-                            className={(link) =>
-                              `group flex items-center px-2 py-2 text-base font-medium rounded-md ${
-                                link?.isActive
-                                  ? "bg-gray-900 text-white"
-                                  : "text-gray-300 hover:bg-gray-700 hover:text-white"
-                              }`
-                            }
+                            className={`group flex items-center px-2 py-2 text-base font-medium rounded-md ${
+                              pathname ==
+                              `/user${navLink?.path ? `/${navLink?.path}` : ""}`
+                                ? "bg-gray-900 text-white"
+                                : "text-gray-300 hover:bg-gray-700 hover:text-white"
+                            }`}
                           >
                             <navLink.icon className="mr-3 flex-shrink-0 h-6 w-6" />
                             {navLink?.name}
@@ -123,13 +123,12 @@ const Sidebar = ({ sidebarOpen, setSidebarOpen }) => {
                     <NavLink
                       key={navLink.name}
                       to={navLink?.path ?? "/user"}
-                      className={(link) =>
-                        `group flex items-center px-2 py-2 text-sm font-medium rounded-md ${
-                          link?.isActive
-                            ? "bg-gray-900 text-white"
-                            : "text-gray-300 hover:bg-gray-700 hover:text-white"
-                        }`
-                      }
+                      className={`group flex items-center px-2 py-2 text-sm font-medium rounded-md ${
+                        pathname ==
+                        `/user${navLink?.path ? `/${navLink?.path}` : ""}`
+                          ? "bg-gray-900 text-white"
+                          : "text-gray-300 hover:bg-gray-700 hover:text-white"
+                      }`}
                     >
                       <navLink.icon className="mr-3 flex-shrink-0 h-6 w-6" />
                       {navLink?.name}
